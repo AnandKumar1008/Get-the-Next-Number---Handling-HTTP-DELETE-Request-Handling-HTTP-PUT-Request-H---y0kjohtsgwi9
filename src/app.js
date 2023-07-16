86fc21a5a3d99747c4bb93a9d3942472fa8a7772
Anand Kumar <121444538+AnandKumar1008@users.noreply.github.com>
2023-07-16 21:35:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //Middlewares
@@ -7,5 +7,14 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
-
+app.get("/api/get-next-num", (req, res) => {
+  // console.log(first)
+  console.log(req.body.num);
+  const num = req.body.num;
+  if (typeof num === "number") {
+    res.status(200).json({ message: 11, status: "success" });
+  } else {
+    res.status(400).json({ status: "failure" });
+  }
+});
 module.exports = app;
